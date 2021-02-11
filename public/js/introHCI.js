d984295f6f620b38259b7d7d84a844c1c95749ba
Jennifer Qi <jeqi@ucsd.edu>
2021-02-11 10:52:03 -0800

    did Lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,10 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("http://localhost:3000/project/", callBackFn);
+}
+
+function callBackFn() {
+
 }
